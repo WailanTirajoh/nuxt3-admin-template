7f214cf7eb40ce9820decc6441aa8f07f1cd9d8d
Wailan Tirajoh <wailantirajoh@gmail.com>
2022-12-01 22:18:06 +0700

    feat: add page transition

--- app.vue
+++ app.vue
@@ -3,3 +3,15 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.3s;
+}
+
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(0.25rem);
+}
+</style>
