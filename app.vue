b3b258fdbe1eaf7eb4792d941b12ef6190e5c099
Wailan Tirajoh <wailantirajoh@gmail.com>
2022-12-02 11:58:31 +0700

    chore: remove filter blur page transition

--- app.vue
+++ app.vue
@@ -6,12 +6,11 @@
 <style>
 .page-enter-active,
 .page-leave-active {
-  transition: all 0.3s;
+  transition: all 0.2s;
 }
 
 .page-enter-from,
 .page-leave-to {
   opacity: 0;
-  filter: blur(0.25rem);
 }
 </style>
