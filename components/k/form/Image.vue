d0ed8f9a816e856c9ea494dd95d782718c160c92
Wailan Tirajoh <wailantirajoh@gmail.com>
2022-12-03 16:46:35 +0700

    chore(style): change image hover bg

--- components/k/form/Image.vue
+++ components/k/form/Image.vue
@@ -43,9 +43,9 @@ const removeImage = () => {
         </button>
       </div>
     </div>
-    <div v-else class="flex w-full items-center justify-center bg-grey-lighter">
+    <div v-else class="flex w-full items-center justify-center">
       <label
-        class="transition-all duration-300 ease-in-out w-full flex flex-col items-center px-4 py-6 bg-white dark:bg-gray-800 dark:hover:bg-gray-900 dark:border-gray-700 dark:text-gray-300 text-gray-400 rounded-lg tracking-wide uppercase border cursor-pointer hover:bg-gray-600 hover:text-white">
+        class="transition-all duration-300 ease-in-out w-full flex flex-col items-center px-4 py-6 bg-white dark:bg-gray-800 dark:hover:bg-gray-900 dark:border-gray-700 dark:text-gray-300 text-gray-400 rounded-lg tracking-wide uppercase border cursor-pointer hover:bg-gray-800 hover:text-white">
         <svg class="w-8 h-8" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
           <path
             d="M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z" />
