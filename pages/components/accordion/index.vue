34c214246615fb47581df3a978c9afafce1c64a8
Wailan Tirajoh <53980548+WailanTirajoh@users.noreply.github.com>
2022-12-29 21:18:31 +0700

    feat: add accordion page

---
+++ pages/components/accordion/index.vue
@@ -0,0 +1,121 @@
+<script setup lang="ts">
+import { TwAccordion, TwButton } from "vue3-tailwind";
+
+useHead({
+  title: "Accordions",
+});
+
+const accordion = [
+  {
+    ref: "example1",
+  },
+  {
+    ref: "example2",
+  },
+];
+</script>
+
+<template>
+  <div class="">
+    <h1 class="text-3xl font-bold">Accordion</h1>
+    <hr class="my-2 border" />
+    <div class="grid gap-2">
+      <div class="col-span-12">
+        <h2 class="mb-2 text-lg font-bold">Example 1:</h2>
+        <TwAccordion class="bg-white rounded overflow-hidden" :data="accordion">
+          <template #header_example1="{ toggleAccordion, ref, isActive }">
+            <div
+              class="bg-sky-100 text-sky-600 p-4 flex justify-between items-center cursor-pointer"
+              @click="toggleAccordion(ref)"
+            >
+              <div>Header</div>
+              <button class="flex items-center">
+                <TwFeather
+                  class="transition-all duration-400"
+                  type="chevron-down"
+                  :class="{
+                    'rotate-180': isActive,
+                  }"
+                ></TwFeather>
+              </button>
+            </div>
+          </template>
+          <template #example1>
+            <div class="p-4">Example 1</div>
+          </template>
+          <template #header_example2="{ toggleAccordion, ref, isActive }">
+            <div
+              class="bg-sky-100 text-sky-600 p-4 flex justify-between items-center cursor-pointer"
+              @click="toggleAccordion(ref)"
+            >
+              <div>Header 2</div>
+              <button class="flex items-center">
+                <TwFeather
+                  class="transition-all duration-400"
+                  type="chevron-down"
+                  :class="{
+                    'rotate-180': isActive,
+                  }"
+                ></TwFeather>
+              </button>
+            </div>
+          </template>
+          <template #example2>
+            <div class="p-4">Example 2</div>
+          </template>
+        </TwAccordion>
+      </div>
+      <div class="col-span-12">
+        <h2 class="mb-2 text-lg font-bold">Example 2:</h2>
+        <div class="">keep-alive</div>
+        <TwAccordion
+          class="bg-white rounded overflow-hidden"
+          :data="accordion"
+          keep-alive
+        >
+          <template #header_example1="{ toggleAccordion, ref, isActive }">
+            <div
+              class="bg-sky-100 text-sky-600 p-4 flex justify-between items-center cursor-pointer"
+              @click="toggleAccordion(ref)"
+            >
+              <div>Header</div>
+              <button class="flex items-center">
+                <TwFeather
+                  class="transition-all duration-400"
+                  type="chevron-down"
+                  :class="{
+                    'rotate-180': isActive,
+                  }"
+                ></TwFeather>
+              </button>
+            </div>
+          </template>
+          <template #example1>
+            <div class="p-4">Example 1</div>
+          </template>
+          <template #header_example2="{ toggleAccordion, ref, isActive }">
+            <div
+              class="bg-sky-100 text-sky-600 p-4 flex justify-between items-center cursor-pointer"
+              @click="toggleAccordion(ref)"
+            >
+              <div>Header 2</div>
+              <button class="flex items-center">
+                <TwFeather
+                  class="transition-all duration-400"
+                  type="chevron-down"
+                  :class="{
+                    'rotate-180': isActive,
+                  }"
+                ></TwFeather>
+              </button>
+            </div>
+          </template>
+          <template #example2>
+            <div class="p-4">Example 2</div>
+          </template>
+        </TwAccordion>
+      </div>
+    </div>
+    <div class="flex gap-2 mb-2"></div>
+  </div>
+</template>
--- store/data/sidebarItem.ts
+++ store/data/sidebarItem.ts
@@ -1,190 +1,196 @@
 export default [
   {
-    "isTitle": true,
-    "name": "Menu",
-    "url": "",
-    "icon": "",
-    "submenu": []
+    isTitle: true,
+    name: "Menu",
+    url: "",
+    icon: "",
+    submenu: [],
   },
   {
-    "isTitle": false,
-    "name": "Dashboard",
-    "url": "/",
-    "icon": "home",
-    "submenu": []
+    isTitle: false,
+    name: "Dashboard",
+    url: "/",
+    icon: "home",
+    submenu: [],
   },
   {
-    "isTitle": false,
-    "name": "Components",
-    "key": "component",
-    "icon": "box",
-    "submenu": [
+    isTitle: false,
+    name: "Components",
+    key: "component",
+    icon: "box",
+    submenu: [
       {
-        "isTitle": false,
-        "name": "Modal",
-        "url": "/components/modal",
-        "icon": "chevron-right",
-        "submenu": []
+        isTitle: false,
+        name: "Accordion",
+        url: "/components/accordion",
+        icon: "chevron-right",
+        submenu: [],
       },
       {
-        "isTitle": false,
-        "name": "Offcanvas",
-        "url": "/components/offcanvas",
-        "icon": "chevron-right",
-        "submenu": []
+        isTitle: false,
+        name: "Button",
+        url: "/components/button",
+        icon: "chevron-right",
+        submenu: [],
       },
       {
-        "isTitle": false,
-        "name": "Button",
-        "url": "/components/button",
-        "icon": "chevron-right",
-        "submenu": []
+        isTitle: false,
+        name: "Modal",
+        url: "/components/modal",
+        icon: "chevron-right",
+        submenu: [],
       },
       {
-        "isTitle": false,
-        "name": "Dropdown",
-        "url": "/components/dropdown",
-        "icon": "chevron-right",
-        "submenu": []
+        isTitle: false,
+        name: "Offcanvas",
+        url: "/components/offcanvas",
+        icon: "chevron-right",
+        submenu: [],
       },
       {
-        "isTitle": false,
-        "name": "Toast",
-        "url": "/components/toast",
-        "icon": "chevron-right",
-        "submenu": []
+        isTitle: false,
+        name: "Dropdown",
+        url: "/components/dropdown",
+        icon: "chevron-right",
+        submenu: [],
       },
       {
-        "isTitle": false,
-        "name": "Icons",
-        "url": "/components/icon",
-        "icon": "chevron-right",
-        "submenu": []
+        isTitle: false,
+        name: "Toast",
+        url: "/components/toast",
+        icon: "chevron-right",
+        submenu: [],
       },
       {
-        "isTitle": false,
-        "name": "Skeleton",
-        "url": "/components/skeleton",
-        "icon": "chevron-right",
-        "submenu": []
+        isTitle: false,
+        name: "Icons",
+        url: "/components/icon",
+        icon: "chevron-right",
+        submenu: [],
       },
       {
-        "isTitle": false,
-        "name": "Tabs",
-        "url": "/components/tabs",
-        "icon": "chevron-right",
-        "submenu": []
+        isTitle: false,
+        name: "Skeleton",
+        url: "/components/skeleton",
+        icon: "chevron-right",
+        submenu: [],
       },
-    ]
+      {
+        isTitle: false,
+        name: "Tabs",
+        url: "/components/tabs",
+        icon: "chevron-right",
+        submenu: [],
+      },
+    ],
   },
   {
-    "isTitle": false,
-    "name": "Inbox",
-    "url": "/inbox",
-    "icon": "inbox",
-    "submenu": []
+    isTitle: false,
+    name: "Inbox",
+    url: "/inbox",
+    icon: "inbox",
+    submenu: [],
   },
   {
-    "isTitle": true,
-    "name": "Form & Tables",
-    "url": "",
-    "icon": "",
-    "submenu": []
+    isTitle: true,
+    name: "Form & Tables",
+    url: "",
+    icon: "",
+    submenu: [],
   },
   {
-    "isTitle": false,
-    "name": "Forms",
-    "key": "forms",
-    "icon": "sidebar",
-    "submenu": [
+    isTitle: false,
+    name: "Forms",
+    key: "forms",
+    icon: "sidebar",
+    submenu: [
       {
-        "isTitle": false,
-        "name": "General",
-        "url": "/form/general",
-        "icon": "chevron-right",
-        "submenu": []
+        isTitle: false,
+        name: "General",
+        url: "/form/general",
+        icon: "chevron-right",
+        submenu: [],
       },
       {
-        "isTitle": false,
-        "name": "Editor",
-        "key": "editor",
-        "icon": "sidebar",
-        "submenu": [
+        isTitle: false,
+        name: "Editor",
+        key: "editor",
+        icon: "sidebar",
+        submenu: [
           {
-            "isTitle": false,
-            "name": "Tiptap",
-            "url": "/form/editor/tiptap",
-            "icon": "chevron-right",
-            "submenu": []
+            isTitle: false,
+            name: "Tiptap",
+            url: "/form/editor/tiptap",
+            icon: "chevron-right",
+            submenu: [],
           },
           {
-            "isTitle": false,
-            "name": "Quill",
-            "url": "/form/editor/quill",
-            "icon": "chevron-right",
-            "submenu": []
+            isTitle: false,
+            name: "Quill",
+            url: "/form/editor/quill",
+            icon: "chevron-right",
+            submenu: [],
           },
-        ]
-      }
-    ]
+        ],
+      },
+    ],
   },
   {
-    "isTitle": false,
-    "name": "Table",
-    "key": "table",
-    "icon": "table",
-    "submenu": [
+    isTitle: false,
+    name: "Table",
+    key: "table",
+    icon: "table",
+    submenu: [
       {
-        "isTitle": false,
-        "name": "Datatable Clientside",
-        "url": "/table/simple",
-        "icon": "chevron-right",
-        "submenu": []
+        isTitle: false,
+        name: "Datatable Clientside",
+        url: "/table/simple",
+        icon: "chevron-right",
+        submenu: [],
       },
       {
-        "isTitle": false,
-        "name": "Datatable Serverside",
-        "url": "/table/datatable",
-        "icon": "chevron-right",
-        "submenu": []
+        isTitle: false,
+        name: "Datatable Serverside",
+        url: "/table/datatable",
+        icon: "chevron-right",
+        submenu: [],
       },
-    ]
+    ],
   },
   {
-    "isTitle": true,
-    "name": "Pages",
-    "url": "",
-    "icon": "",
-    "submenu": [
-    ]
+    isTitle: true,
+    name: "Pages",
+    url: "",
+    icon: "",
+    submenu: [],
   },
   {
-    "isTitle": false,
-    "name": "Page",
-    "url": "",
-    "icon": "sidebar",
-    "submenu": [
+    isTitle: false,
+    name: "Page",
+    url: "",
+    icon: "sidebar",
+    submenu: [
       {
-        "isTitle": false,
-        "name": "Login",
-        "url": "/login",
-        "icon": "chevron-right",
-        "submenu": []
+        isTitle: false,
+        name: "Login",
+        url: "/login",
+        icon: "chevron-right",
+        submenu: [],
       },
       {
-        "isTitle": false,
-        "name": "Register",
-        "url": "/register",
-        "icon": "chevron-right",
-        "submenu": []
+        isTitle: false,
+        name: "Register",
+        url: "/register",
+        icon: "chevron-right",
+        submenu: [],
       },
       {
-        "isTitle": false,
-        "name": "Profile",
-        "url": "/profile",
-        "icon": "chevron-right",
-        "submenu": []
+        isTitle: false,
+        name: "Profile",
+        url: "/profile",
+        icon: "chevron-right",
+        submenu: [],
       },
-    ]
+    ],
   },
-]
+];
