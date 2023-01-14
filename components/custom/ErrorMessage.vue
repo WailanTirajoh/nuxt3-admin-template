a943b3e1f8443cc46bdf51646659a853c736014c
Wailan Tirajoh <53980548+WailanTirajoh@users.noreply.github.com>
2023-01-14 20:36:49 +0700

    feat: add custom error message

---
+++ components/custom/ErrorMessage.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { TwErrorMessage } from "vue3-tailwind";
+
+defineProps<{
+  name: string;
+}>();
+</script>
+
+<template>
+  <TwErrorMessage :name="name">
+    <template v-slot="{ errors }">
+      <!-- <div v-if="errors"> -->
+        <div class="text-gray-600 dark:text-gray-400 normal-case text-sm" v-if="errors">Please resolve:</div>
+        <ul class="list-disc text-red-500 text-sm italic pl-6">
+          <li v-for="(error, index) in errors" :key="`${name}-${index}`">
+            {{ error }}
+          </li>
+        </ul>
+      <!-- </div> -->
+    </template>
+  </TwErrorMessage>
+</template>
--- pages/form/general.vue
+++ pages/form/general.vue
@@ -7,7 +7,6 @@ import {
   TwMultiSelect,
   TwSelect,
   TwToggle,
-  TwErrorMessage,
   TwTextarea,
 } from "vue3-tailwind";
 import { useFormExample } from "@/store/formExample";
@@ -33,54 +32,52 @@ const formExample = useFormExample();
         </div>
         <div class="col-span-12">
           <TwInput
-            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-400 focus:ring-indigo-400 focus:ring-1 sm:text-sm"
+            label="Input"
             name="inputExample"
             v-model="formExample.formData.inputExample"
-            label="Input"
             placeholder="Input Field"
             type="text"
           />
-          <TwErrorMessage name="inputExample" />
+          <CustomErrorMessage name="inputExample" />
         </div>
         <div class="col-span-12">
           <TwTextarea
-            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-400 focus:ring-indigo-400 focus:ring-1 sm:text-sm"
+            label="Textarea"
             name="textAreaExample"
             v-model="formExample.formData.textAreaExample"
-            label="Textarea"
             placeholder="Textarea Field"
             type="text"
           />
-          <TwErrorMessage name="textAreaExample" />
+          <CustomErrorMessage name="textAreaExample" />
         </div>
         <div class="col-span-12">
           <TwSelect
+            label="Single Select"
             name="selectExample"
             v-model="formExample.formData.selectExample"
             :items="formExample.selectionList"
-            label="Single Select"
             placeholder="Choose select"
           />
-          <TwErrorMessage name="selectExample" />
+          <CustomErrorMessage name="selectExample" />
         </div>
         <div class="col-span-12">
           <TwMultiSelect
+            label="Multi Select"
             name="multiSelectExample"
             v-model="formExample.formData.multiSelectExample"
             :items="formExample.selectionList"
-            label="Multi Select"
             placeholder="Choose select"
           />
-          <TwErrorMessage name="multiSelectExample" />
+          <CustomErrorMessage name="multiSelectExample" />
         </div>
         <div class="col-span-12">
           <TwToggle
+            label="Toggle"
             name="toggleExample"
             id="toggle"
             v-model="formExample.formData.toggleExample"
-            label="Toggle"
           />
-          <TwErrorMessage name="toggleExample" />
+          <CustomErrorMessage name="toggleExample" />
         </div>
         <div class="col-span-12 flex justify-end gap-1">
           <TwButton
