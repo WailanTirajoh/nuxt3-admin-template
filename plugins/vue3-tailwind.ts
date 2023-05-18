81d6d90048cc5fca15d5fb2c6d58661a849a8af5
Wailan Tirajoh <53980548+WailanTirajoh@users.noreply.github.com>
2023-05-18 14:05:19 +0700

    chore(deps): bump vue3-tailwind.

--- plugins/vue3-tailwind.ts
+++ plugins/vue3-tailwind.ts
@@ -1,10 +1,13 @@
-import { TwRipple, useForm } from "vue3-tailwind";
+import { TwClickOutside, TwRipple, useForm } from "vue3-tailwind";
 
 export default defineNuxtPlugin((nuxtApp) => {
   // Activate v-ripple directive
   TwRipple.color = "rgba(255, 255, 255, 0.35)";
   TwRipple.zIndex = "55";
   nuxtApp.vueApp.directive("ripple", TwRipple);
+
+  // Activate click outside directive
+  nuxtApp.vueApp.directive("click-outside", TwClickOutside);
 
   // Customize Form Rules
   // You can create new rules here / overwrite the default rules
