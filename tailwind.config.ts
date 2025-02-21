09567855ebc310d05c3b33741106a1ea1418d67b
Wailan Tirajoh <wailantirajoh@gmail.com>
2025-02-21 19:41:23 +0700

    feat(app): update tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+export default <Partial<Config>>{
   content: [
     "./components/**/*.{js,vue,ts}",
     "./layouts/**/*.vue",
@@ -7,12 +8,10 @@ module.exports = {
     "./plugins/**/*.{js,ts}",
     "./nuxt.config.{js,ts}",
     "./app.vue",
+    "./tailwind-theme.js",
   ],
   darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {},
   },
-  plugins: [
-    require("@tailwindcss/typography")
-  ],
-}
+};
